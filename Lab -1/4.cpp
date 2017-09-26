282de4aa26824f59dfc005aa5afabe497db99f43
Furtsev Denis <31769435+Gorand174@users.noreply.github.com>
2017-09-26 22:43:59 +0600

    4
    Оно даже работает

---
+++ Lab -1/4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double who = 0;
+	double doc = 0;
+	while (1)
+	{
+		double tor;
+		cout << "Ввод числа/'0' для завершения: ";
+		cin >> tor;
+		if (tor == 0)
+			break;
+		if (tor > 0)
+		{
+			who += tor;
+			doc++;
+		}
+	}
+	double i;
+	i = who / doc;
+	cout << i << " ";
+
+	system("pause");
+	return 0;
+}
