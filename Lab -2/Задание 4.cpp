b5dc977731d2491abacf6db8183cfdfc67cda381
Furtsev Denis <31769435+Gorand174@users.noreply.github.com>
2017-10-17 18:03:06 +0600

    Add files via upload

---
+++ Lab -2/Задание 4.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Введите размерность квадратной матрицы, а затем все её элементы: ";
+	cin >> n;
+
+	int **x = new int*[n];
+	int *y = new int[2 * n];
+
+
+	for (int i = 0; i < n; i++)
+	{
+		x[i] = new int[n];
+		for (int j = 0; j < n; j++)
+			cin >> x[i][j];
+	}
+
+
+	int k = 0, sum;
+	for (int i = n - 1; i > 0; i--)
+	{
+		sum = 0;
+		for (int j = i; j < n; j++)
+			sum += x[j][j - i];
+		y[k++] = sum;
+	}
+	
+
+	for (int i = 0; i < n; i++)
+	{
+		sum = 0;
+		for (int j = i; j < n; j++)
+			sum += x[j - i][j];
+		y[k++] = sum;
+	}
+
+	cout << "Суммы по диагоналям: ";
+	for (int i = 0; i < 2 * n - 1; i++)
+		cout << y[i] << " ";
+
+	system("pause");
+	delete[]x;
+	delete[]y;
+	return 0;
+}
